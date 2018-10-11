45af3e14da0a07e45cd1e7b2e02955a7eb5da087
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 15:06:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteers) # code an argument here
   # Your code here
+  planeteers.map do |planeteer|
+    planeteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteers) # code an argument here
   # Your code here
+  planeteers.each do |planeteer|
+    
 end
 
 def find_the_cheese# code an argument here
